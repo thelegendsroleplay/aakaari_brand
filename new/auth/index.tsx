c7195ab53af41b6def0f7cf6db4ea7a6dcc28030
thelegendsroleplay <bhoslenikhil0@gmail.com>
2025-11-06 21:04:01 +0530

    small update

--- new/auth/index.tsx
+++
@@ -1,3 +0,0 @@
-export { LoginPageContainer } from './LoginPageContainer';
-export { SignUpPageContainer } from './SignUpPageContainer';
-export { ForgotPasswordPage } from '../../components/auth/ForgotPasswordPage';
